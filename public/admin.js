4b21b2bb1971049357832e9417514e60b450f52d
Dominik Widomski <dominik@digital-detox.co.uk>
2016-05-26 11:02:22 +0100

    Create simple angular controller

--- public/admin.js
+++ public/admin.js
@@ -14,3 +14,24 @@ socket.on('usersInfo', data => {
 socket.on('newUserView', data => {
 	console.log("NEW USER VIEW", data);
 });
+
+angular.module('adminApp', [])
+	.controller('UsersController', function($scope) {
+		$scope.users = [
+			{
+				username: 'dom',
+			},
+			{
+				username: 'kacper',
+			}
+		];
+
+
+		$scope.addNewUser = function() {
+			$scope.users.push({
+				username: $scope.newUserName
+			});
+
+			$scope.newUserName = "";
+		}
+	});
